cc6b5e63b436d8730e6add72af2c4330dc60e5bd
Gang Li <ligangty@users.noreply.github.com>
2026-05-07 10:08:51 +0800

    simple fix

--- src/main/java/org/commonjava/aws/efs/EFSCopyFileCmd.java
+++ src/main/java/org/commonjava/aws/efs/EFSCopyFileCmd.java
@@ -27,7 +27,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.IntStream;
 
